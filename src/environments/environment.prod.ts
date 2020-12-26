c8821cafd78d3d7d6d1665b4ef537fef611a7f64
amol <amol.borgaonkar@workex.xyz>
2020-12-26 08:05:23 +0530

    generated deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBase: 'https://deltaprop.herokuapp.com/',
+  auth0: {
+    url: 'project-bookmark.us', // the auth0 domain prefix
+    audience: 'http://localhost:5000', // the audience set for the auth0 app
+    clientId: 'gmdRc7yb4eUpme3tXN2R8r67LVjSVPUw', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:4200/list', // the base url of the running ionic application.
+  }
 };
